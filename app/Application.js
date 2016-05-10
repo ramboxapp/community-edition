d49a788e26dc59183f651cb72cdd8b8a3539f97e
saenzramiro <saenzramiro@gmail.com>
2016-05-10 18:41:32 -0300

    Shortcuts to switch services
    Added shortcuts to switch between services using CTRL + Number.

--- app/Application.js
+++ app/Application.js
@@ -13,6 +13,14 @@ Ext.define('Rambox.Application', {
 	}
 
 	,launch: function () {
-
+		// Add shortcuts to switch services using CTRL + Number
+		var map = new Ext.util.KeyMap({
+			 target: document
+			,key: "0123456789"
+			,ctrl: true
+			,fn: function(key) {
+				Ext.cq1('app-main').setActiveTab(key - 48);
+			}
+		});
 	}
 });
