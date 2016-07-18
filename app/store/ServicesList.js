ec52c9e5c63aac6d5e5c690be466fa235aee88bb
Ramiro Saenz <saenzramiro@gmail.com>
2016-07-18 20:12:51 -0300

    Fixes #78

--- app/store/ServicesList.js
+++ app/store/ServicesList.js
@@ -78,8 +78,9 @@ Ext.define('Rambox.store.ServicesList', {
 			,logo: 'hipchat.png'
 			,name: 'HipChat'
 			,description: 'HipChat is hosted group chat and video chat built for teams. Supercharge real-time collaboration with persistent chat rooms, file sharing, and screen sharing.'
-			,url: 'https://www.hipchat.com/sign_in/'
+			,url: 'https://___.hipchat.com/'
 			,type: 'messaging'
+			,js_unread: 'function checkUnread(){var e=document.getElementsByClassName("hc-badge"),t=0;for(i=0;i<e.length;i++)t+=parseInt(e[i].innerHTML.trim());updateBadge(t)}function updateBadge(e){e>=1?document.title="("+e+") "+originalTitle:document.title=originalTitle}var originalTitle=document.title;setInterval(checkUnread,3000);'
 		},
 		{
 			 id: 'telegram'
