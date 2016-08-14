387f470744a10a265d08360eb5cd0ee9e7435601
TriiNoxYs <triinoxys@gmail.com>
2016-08-14 14:46:54 +0200

    Edit Zohochat description

--- app/store/ServicesList.js
+++ app/store/ServicesList.js
@@ -403,7 +403,7 @@ Ext.define('Rambox.store.ServicesList', {
 			 id: 'zohochat'
 			,logo: 'zohochat.png'
 			,name: 'Zoho Chat'
-			,description: 'Free and open source webmail software for the masses, written in PHP.'
+			,description: 'Free and open source chat software for the masses, written in PHP.'
 			,url: 'https://chat.zoho.com/'
 			,type: 'messaging'
 			,js_unread: 'NotifyByTitle.show = function(){};NotifyByTitle.start = function(){};NotifyByTitle.stop = function(){};function checkUnread(){var t=0;$(".msgnotify").each(function() { t += isNaN(parseInt($(this).html())) ? 0 : parseInt(parseInt($(this).html())) });updateBadge(t)}function updateBadge(e){e>=1?document.title="("+e+") "+originalTitle:document.title=originalTitle}var originalTitle=document.title;setInterval(checkUnread,3000);'
