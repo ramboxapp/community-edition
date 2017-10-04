9e43572162002eed6bf74091b59b34a82699a91f
Ramiro Saenz <saenzramiro@gmail.com>
2017-10-04 17:19:19 -0300

    Allow http in https services

--- app/ux/WebView.js
+++ app/ux/WebView.js
@@ -174,7 +174,7 @@ Ext.define('Rambox.ux.WebView',{
 					,plugins: 'true'
 					,allowtransparency: 'on'
 					,autosize: 'on'
-					//,webpreferences: 'nodeIntegration=no'
+					,webpreferences: 'allowRunningInsecureContent=yes' //,nativeWindowOpen=yes
 					//,disablewebsecurity: 'on' // Disabled because some services (Like Google Drive) dont work with this enabled
 					,useragent: Ext.getStore('ServicesList').getById(me.record.get('type')).get('userAgent')
 					,preload: './resources/js/rambox-service-api.js'
