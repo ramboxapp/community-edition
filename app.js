b49b1704ba8f38950f7e39350f15046e3ad2a9cb
NickMcSweeney <shindler25@gmail.com>
2018-01-27 16:18:15 -0800

    add coment about building themes. removed log

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var auth0, lock; // Auth0 vars
 
 // load theme
-console.log(localStorage.rambox_theme, Ext.isDebugEnabled);
 if (localStorage.rambox_theme !== "default") Ext.util.CSS.swapStyleSheet("rambox-default-theme", `build/${localStorage.rambox_theme}/production/Rambox/resources/Rambox-all.css`);
 
 // Enable Cookies
