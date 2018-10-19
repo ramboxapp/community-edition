248fad7b2a71c5e104a3f26d13d9bac45e32a586
brad <bradpitcher@gmail.com>
2018-10-18 21:20:23 -0600

    fix #1292, fix #1829 enable video chat in ms teams

--- app/store/ServicesList.js
+++ app/store/ServicesList.js
@@ -690,7 +690,7 @@ Ext.define('Rambox.store.ServicesList', {
 			,description: 'Microsoft Teams is the chat-based workspace in Office 365 that integrates all the people, content, and tools your team needs to be more engaged and effective.'
 			,url: 'https://teams.microsoft.com'
 			,type: 'messaging'
-			,userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.2883.87 Safari/537.36'
+			,userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.2883.87 Safari/537.36 Edge/12.246'
 		},
 		{
 			 id: 'kezmo'
