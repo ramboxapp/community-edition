085f9a7c77e58d2cbe48cdf82c0b1aa11eff1259
TheGoddessInari <thegoddessinari@gmail.com>
2019-01-11 13:17:02 -0800

    Update to electron 4.0.1 and electron-builder 20.38.5.

--- electron/main.js
+++ electron/main.js
@@ -312,7 +312,8 @@ ipcMain.on('relaunchApp', function(event) {
 	app.exit(0);
 });
 
-const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
+const haveLock = app.requestSingleInstanceLock();
+app.on('second-instance', (commandLine, workingDirectory) => {
 	// Someone tried to run a second instance, we should focus our window.
 	if (mainWindow) {
 		if (mainWindow.isMinimized()) mainWindow.restore();
@@ -323,7 +324,7 @@ const shouldQuit = app.makeSingleInstance((commandLine, workingDirectory) => {
 	}
 });
 
-if (shouldQuit) {
+if (!haveLock) {
 	app.exit();
 }
 
